0606487d13f7566cb334d111978cb902e28a17c2
Cynthia Cruz <cynthia.cruzoliveira@gmail.com>
2024-02-01 01:03:03 -0300

    Inicio da página em react - imagem e opções do restaurante

---
+++ hashtaurante/src/App.jsx
@@ -0,0 +1,10 @@
+import Hashtaurante from "./assets/hashtaurante.webp";
+import "./App.css";
+import { Navegacao } from "./Navegacao";
+
+export function App() {
+  return <>
+          <img src={Hashtaurante} alt="" className="capa"/>
+          <Navegacao />
+         </>
+}
