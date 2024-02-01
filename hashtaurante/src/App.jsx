76784da7d72b0d8b2f6f72ab6ab2332901a49998
Cynthia Cruz <cynthia.cruzoliveira@gmail.com>
2024-02-01 15:56:42 -0300

    Finalização do projeto

--- hashtaurante/src/App.jsx
+++ hashtaurante/src/App.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import Hashtaurante from "./assets/hashtaurante.webp";
 import "./App.css";
 import { Navegacao } from "./Navegacao";
+import { ItemCardapio } from "./ItemCardapio";
+import { pratosPrincipais, sobremesas, bebidas } from "./cardapio";
 
 export function App() {
+  const paginasMenus = [pratosPrincipais, sobremesas, bebidas];
+  const [paginaSelecionada, atualizarPaginaSelecionada] = useState(0);
+
   return <>
           <img src={Hashtaurante} alt="" className="capa"/>
-          <Navegacao />
+          <Navegacao atualizarPaginaSelecionada={atualizarPaginaSelecionada}/>
+          <div className="menu">
+            { paginasMenus[paginaSelecionada].map(item => 
+              <ItemCardapio  
+                nome={item.nome} 
+                preco={item.preco}
+                descricao={item.descricao}
+                imagem={item.imagem}
+              />) }
+          </div>
          </>
 }
